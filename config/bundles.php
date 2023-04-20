f720d4116aaf23041adfcffadc6a2bc342c25948
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-04-20 18:28:07 +0200

    Update PHP prerequisite to 8.1 and added Api Platform

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\TimeBundle\KnpTimeBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
