91a3275c63983770848373111f7b0b9821a3734b
pentiminax <tanguy.lemarie.youtube@outlook.fr>
2023-05-31 13:59:50 +0200

    Migrate up to Symfony 4.3

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,9 +15,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -25,9 +25,8 @@ class DashboardController extends AbstractDashboardController
         private AdminUrlGenerator $adminUrlGenerator
     ) {}
 
-    /**
-     * @IsGranted("ROLE_AUTHOR")
-     */
+
+    #[IsGranted('ROLE_AUTHOR')]
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
